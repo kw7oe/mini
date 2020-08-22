3fa4fe1842d425c0d5c8d1ff472255cc88df20c7
kw7oe <choongkwern@hotmail.com>
2020-08-22 23:32:16 +0800

    Remove doc test [build]

--- lib/mini.ex
+++ lib/mini.ex
@@ -3,15 +3,6 @@ defmodule Mini do
   Documentation for `Mini`.
   """
 
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> Mini.hello()
-      :world
-
-  """
   def hello do
     :world
   end
